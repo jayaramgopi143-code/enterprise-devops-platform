767185285fe107e332ffcc7d1bfc608bf5e333b0
jayaramgopi143-code <jayaramgopi143@gmail.com>
2026-06-03 07:11:09 +0000

    fixed github ssh access

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "devops_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_allowed_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
